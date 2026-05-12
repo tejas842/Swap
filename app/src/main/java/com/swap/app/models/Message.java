b9b6137a5286f729566e81894a19dfd637b729b7
tejas842 <belsaretejas915@gmail.com>
2026-05-12 10:45:17 +0530

    Add Message class with message properties

---
+++ app/src/main/java/com/swap/app/models/Message.java
@@ -0,0 +1,9 @@
+package com.swap.app.models;
+public class Message {
+    public String id;
+    public String content;
+    public String senderId;
+    public String conversationId;
+    public String createdAt;
+    public String messageType;
+}
