c9a1e9d64666aafda5d5c42bc8efb54747758e0b
tejas842 <belsaretejas915@gmail.com>
2026-05-12 09:55:20 +0530

    Add Community model class with relevant fields

---
+++ app/src/main/java/com/swap/app/models/Community.java
@@ -0,0 +1,11 @@
+package com.swap.app.models;
+public class Community {
+    public String id;
+    public String name;
+    public String description;
+    public String category;
+    public String avatarUrl;
+    public String createdBy;
+    public String createdAt;
+    public int memberCount;
+}
